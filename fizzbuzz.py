3e423a87a063dc2ed018568c35d85e5cbff85d4d
hyan9 <chakyu92@gmail.com>
2025-12-31 09:50:21 +0900

    refactor: if문 로직 최적화

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,2 @@
 for i in range (1, 15+1):
-    if i % 3 == 0 and i % 5 == 0: print("fizzbuzz")
-    elif i % 3 == 0: print("fizz")
-    elif i % 5 == 0: print("buzz")
-    else: print(i)
+    print("fizz"*(i%3==0) + "buzz"*(i%5==0) or i)
